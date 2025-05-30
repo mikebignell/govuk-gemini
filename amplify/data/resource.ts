68948e5ac80cb80fa8edb955f291df3f9d15e09f
Mike Bignell <michael.bignell@accenture.com>
2025-05-30 20:57:46 +0100

    Added top level query to satisfy amplify

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -2,19 +2,20 @@ import { type ClientSchema, defineData, a } from '@aws-amplify/backend';
 import { generatePage } from '../function/generatePage/resource';
 
 const schema = a.schema({
-    generatePage: a
-      .mutation()
-      .arguments({ prompt: a.string() })
-      .returns(a.string())
-      //.authorization(allow => [allow.guest()])
-      .handler(a.handler.function(generatePage))
-  })
+  dummy: a.query().returns(a.string()), // Remove handler, use default resolver
+  generatePage: a
+    .mutation()
+    .arguments({ prompt: a.string() })
+    .returns(a.string())
+    .authorization(allow => [allow.guest()])
+    .handler(a.handler.function(generatePage))
+});
 
-export type Schema = ClientSchema<typeof schema>
+export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'apiKey', // or 'apiKey' for public, or 'userPool' for auth
+    defaultAuthorizationMode: 'apiKey',
   },
 });
